9bcc9395bb95c2d8381eb01b71518bfee00cc510
Martin <bravomartin178@gmail.com>
2021-06-03 22:58:36 -0300

    proyecto web-commit10-2.0-tienda

--- proyectoWebAPP/views.py
+++ proyectoWebAPP/views.py
@@ -5,7 +5,3 @@ from django.shortcuts import render, HttpResponse
 def home(request):
 
     return render(request, "ProyectoWebAPP/home.html")
-
-def tienda(request):
-
-    return render(request, "ProyectoWebAPP/tienda.html")
--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+
+    nombre=models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='categoria'
+        verbose_name_plural='categorias'
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+
+    nombre=models.CharField(max_length=50)
+    precio = models.FloatField()
+    imagen=models.ImageField(upload_to='tienda', null=True, blank=True)
+    disponibilidad = models.BooleanField(default=True)
+    categorias=models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name='producto'
+        verbose_name_plural='productos'
+
+    def __str__(self):
+        return self.nombre

9a1502b30a65998ccc2e67d0738180aabcc262e4
Martin <bravomartin178@gmail.com>
2021-07-02 16:06:26 -0300

    proyecto web-commit11-2.1-carroTienda

--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,19 @@
 from .forms import FormularioContacto
 from django.shortcuts import render, redirect
 from django.core.mail import EmailMessage
+from smtplib import SMTP
 
 # Create your views here.
+
+def enviarCorreo(asunto, mensaje):
+
+    message = 'Subject: {}\n\n{}'.format(asunto, mensaje)
+
+    smtp = SMTP("smtp.gmail.com")
+    smtp.starttls()
+    smtp.login("bravomartin178@gmail.com", "avenged sevenfold")
+    smtp.sendmail("bravomartin178@gmail.com", "bravomartin178@gmail.com", message)
+    smtp.quit()
 
 def contacto(request):
 
@@ -15,14 +26,18 @@ def contacto(request):
             email=request.POST.get("email")
             contenido=request.POST.get("contenido")
 
-            email=EmailMessage("Mensaje de Web Django", 
-            "El usuario {} con la direccion {} escribe lo siguiente:\n\n {}".format(nombre, email, contenido), 
-            "", ["bravomartin178@gmail.com"], reply_to=[email])
+            # email=EmailMessage("Mensaje de Web Django", 
+            # "El usuario {} con la direccion {} escribe lo siguiente:\n\n {}".format(nombre, email1, contenido), 
+            # "", ["bravomartin178@gmail.com"], reply_to=[email1])
+
+            asunto = "Mensaje de la Web Django"
+            mensaje="El usuario {} con la direccion {} escribe lo siguiente:\n\n {}".format(nombre, email, contenido)
 
             # infoForm=formulario_contacto.cleaned_data
             try:
             #     send_mail('Mensaje de Web Django', 'El usuario {} con la direccion {} escribe lo siguiente:\n\n {}'.format(nombre, email, contenido), '', ['bravomartin178@gmail.com'],)
-                email.send()
+                # email.send()
+                enviarCorreo(asunto, mensaje)
 
                 return redirect("/contacto/?valido")
             except:
